40ecac3ea6d5cd6293eb4d766020859f47e0572f
Erin Carroll <erin_carroll@berkeley.edu>
2025-11-19 10:21:25 -0800

    2025 rccs

---
+++ 2025/data_prep/1_radianceH5toEnvi_rdn.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('/store/carroll/repos/chess-isofit/2025/')
+from data_prep import radianceH5toEnvi
+import os
+from glob import glob
+
+home = '/store/carroll/col/data/2025/'
+outDir = os.path.join(home, 'raw/L1/radianceENVI/')
+h5_dir = os.path.join(home, 'raw/L1/radianceH5/')
+h5_files = glob(os.path.join(h5_dir, '*/*/*.h5'))
+
+filePathToEnviProjCs = '/store/shared/ENVI6/envi61/idl/resource/pedata/predefined/EnviPEProjcsStrings.txt'
+
+rasterNames = ['Radiance']
+
+for h5_filename in h5_files:
+    for rasterName in rasterNames:
+        outFile = os.path.join(outDir,os.path.basename(h5_filename).replace('radiance.h5','rdn'))
+        try:
+            radianceH5toEnvi.convertH5RasterToEnvi(h5_filename, rasterName, outFile, filePathToEnviProjCs)
+        except:
+            print('failed', outFile)
